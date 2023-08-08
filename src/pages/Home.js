7908f879dbb7c396bee86d69c1e3f16af228587a
yayalew <yeabsira282@gmail.com>
2023-08-07 20:26:16 -0500

    landing page finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,18 @@
 import React from 'react';
 import Concert from '../assets/SANDMAN GOODWILL NOV 13_Trim.mp4'
 import "../index.css"
+import Sandman from '../assets/sandman-logo.png'
 
 const Home = () => {
     return (
-        <div>
-            <video className='background-video' autoPlay loop muted>
-                <source src={Concert} type="video/mp4" />
-                <div>
-                <h1>Sandmannnnnnnn</h1>
+        <div className="main">
+            <video src={Concert} autoPlay loop muted />
+                <div className="content">
+                <div >
+                    <img src={Sandman} alt="Sandman Logo" className="artist-logo-home"/>
                 </div>
-            </video>
-            
+                {/* <h2> Rock and Roll </h2> */}
+                </div>
         </div>
     );
 };
